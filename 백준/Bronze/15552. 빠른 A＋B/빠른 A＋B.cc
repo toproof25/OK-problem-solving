d0f4d7445803f64010e04aeba2d156083eba12ee
toproof <41888060+toproof25@users.noreply.github.com>
2024-06-17 21:13:35 +0900

    [Bronze IV] Title: 빠른 A+B, Time: 196 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/15552. 빠른 A＋B/빠른 A＋B.cc
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+void func(int number){
+  
+  for (int i=0; i < number; i++){
+    short A, B;
+    cin >> A >> B;
+    cout << A+B << '\n';
+  }
+}
+
+int main(){
+  int number;
+
+	cin.tie(NULL);
+  ios::sync_with_stdio(false);
+
+  cin >> number;
+  func(number);
+
+  return 0;
+}

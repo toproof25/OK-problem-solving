4076b8c29c926e1682ab6a65407c730847d24b19
toproof <41888060+toproof25@users.noreply.github.com>
2024-07-22 17:04:55 +0900

    [level 0] Title: 뒤에서 5등 위로, Time: 0.02 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ 프로그래머스/0/181852. 뒤에서 5등 위로/뒤에서 5등 위로.cpp
@@ -0,0 +1,34 @@
+#include <string>
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+void seletion_sort(vector<int> &v){
+  for (int i = 0; i < v.size()-1; ++i)
+  {
+    int low = i;
+    for (int j = i+1; j < v.size(); ++j)
+    {
+      if (v[j] < v[low])
+      {
+        low = j;
+      }
+    }
+    
+    int temp = v[i];
+    v[i] = v[low];
+    v[low] = temp;
+  }
+}
+
+vector<int> solution(vector<int> num_list) {
+    vector<int> answer;
+
+    seletion_sort(num_list);
+    for (int i=5; i<num_list.size(); i++)
+    {
+      answer.push_back(num_list[i]);
+    }
+    return answer;
+}

affab93fb16e1c00e54ba47f99bc68aa8ab411df
toproof <41888060+toproof25@users.noreply.github.com>
2024-07-24 09:43:46 +0900

    [level 0] Title: 수 조작하기 2, Time: 1.57 ms, Memory: 6.82 MB -BaekjoonHub

---
+++ 프로그래머스/0/181925. 수 조작하기 2/수 조작하기 2.cpp
@@ -0,0 +1,27 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(vector<int> numLog) {
+    string answer = "";
+    answer.reserve(numLog.size()-1);
+    
+    for (int i=1; i<numLog.size(); ++i)
+    {
+        int isAlpha = numLog[i] - numLog[i-1];
+        
+        if (isAlpha == 1)
+            answer += 'w';
+        else if (isAlpha == -1)
+            answer += 's';
+        else if (isAlpha == 10)
+            answer += 'd';
+        else if (isAlpha == -10)
+            answer += 'a';
+    }
+    
+    
+    return answer;
+}
+

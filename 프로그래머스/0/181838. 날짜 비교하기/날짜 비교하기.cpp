3ecc8db1d750102c59d359e009376921854de5d3
toproof <41888060+toproof25@users.noreply.github.com>
2024-08-02 15:01:11 +0900

    [level 0] Title: 날짜 비교하기, Time: 0.01 ms, Memory: 4.15 MB -BaekjoonHub

---
+++ 프로그래머스/0/181838. 날짜 비교하기/날짜 비교하기.cpp
@@ -0,0 +1,17 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(vector<int> date1, vector<int> date2) {
+    int answer = 0;
+    
+    if (date1[0] < date2[0])
+        answer = 1;
+    else if (date1[0] == date2[0] && date1[1] < date2[1])
+        answer = 1;
+    else if (date1[0] == date2[0] && date1[1] == date2[1] && date1[2] < date2[2])
+        answer = 1;
+    
+    return answer;
+}

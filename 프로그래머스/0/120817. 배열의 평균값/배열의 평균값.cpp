dddd4a1d03fa76ea3e798f5b931863cfdffb0920
toproof <41888060+toproof25@users.noreply.github.com>
2024-08-04 12:28:09 +0900

    [level 0] Title: 배열의 평균값, Time: 0.01 ms, Memory: 3.75 MB -BaekjoonHub

---
+++ 프로그래머스/0/120817. 배열의 평균값/배열의 평균값.cpp
@@ -0,0 +1,16 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+double solution(vector<int> numbers) {
+    double answer = 0;
+
+    int size = numbers.size();
+    for (int i=0; i<size; ++i) {
+        answer += numbers[i];
+    }
+    answer /= size;
+    
+    return answer;
+}

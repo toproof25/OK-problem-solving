b3f30478a3cf70f902aaf59324d9994ce15d4e5c
toproof <41888060+toproof25@users.noreply.github.com>
2024-08-17 14:26:30 +0900

    [level 1] Title: 부족한 금액 계산하기, Time: 0.01 ms, Memory: 4.15 MB -BaekjoonHub

--- 프로그래머스/1/82612. 부족한 금액 계산하기/부족한 금액 계산하기.cpp
+++ 프로그래머스/1/82612. 부족한 금액 계산하기/부족한 금액 계산하기.cpp
@@ -2,12 +2,12 @@ using namespace std;
 
 long long solution(int price, int money, int count)
 {
-    long long answer = 0;
+    long long answer;
     
-    for (int i=1; i<=count; ++i)
-        answer += price*i;
+    // 가우스 공식
+    answer = ((long long)price * (count * (count+1) / 2)) - money;
 
-    if (answer-money < 0)
+    if (answer < 0)
         return 0;
-    return answer-money;
+    return answer;
 }

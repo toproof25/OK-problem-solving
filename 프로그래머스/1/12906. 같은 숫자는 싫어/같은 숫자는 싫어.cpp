ed8932fe05760b89484e02bf0d017629edda8ba4
toproof <41888060+toproof25@users.noreply.github.com>
2024-08-20 13:57:08 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 111.30 ms, Memory: 105 MB -BaekjoonHub

---
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.cpp
@@ -0,0 +1,19 @@
+#include <vector>
+#include <iostream>
+#include <stack>
+
+using namespace std;
+
+vector<int> solution(vector<int> arr) 
+{
+    vector<int> answer;
+    answer.reserve(arr.size());
+    
+    for (const int &i : arr)
+    {
+        if (answer.back() != i || answer.empty())
+            answer.push_back(i);
+    }
+
+    return answer;
+}

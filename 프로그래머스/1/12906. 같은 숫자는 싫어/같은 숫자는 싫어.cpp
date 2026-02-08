2a23c54beab17c7cff1b4e152aca452184a9e950
toproof <41888060+toproof25@users.noreply.github.com>
2026-02-08 14:56:35 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 115.78 ms, Memory: 105 MB -BaekjoonHub

--- 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.cpp
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.cpp
@@ -1,6 +1,6 @@
 #include <vector>
 #include <iostream>
-#include <stack>
+#include <queue>
 
 using namespace std;
 
@@ -9,10 +9,15 @@ vector<int> solution(vector<int> arr)
     vector<int> answer;
     answer.reserve(arr.size());
     
+    queue<int> q;
     for (const int i : arr)
+        q.push(i);
+    
+    while(!q.empty())
     {
-        if (answer.back() != i || answer.empty())
-            answer.push_back(i);
+        if (answer.empty() || answer.back() != q.front())
+            answer.push_back(q.front());
+        q.pop();
     }
 
     return answer;
